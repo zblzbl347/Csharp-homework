713f0e6a0d93e240a4d1266f0d62e85224b5cd68
zblzbl347 <61130402+zblzbl347@users.noreply.github.com>
2020-05-26 23:31:14 +0800

    Add files via upload

---
+++ homework1/calculator1/calculator1/Calculate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator1
+{
+    public class Calculate
+    {
+        /// <summary>
+        /// 获取用户输入的值
+        /// </summary>
+        /// <param name="type">计算类型</param>
+        /// <param name="num1">第一个数字</param>
+        /// <param name="num2">第二个数字</param>
+        /// <returns></returns>
+        public int GetResult(string type, int num1,int num2)
+        {
+            int sum = 0;
+            if (type == "+")
+            {
+                sum = num1 + num2;
+            }
+            else if (type == "-")
+            {
+                sum = num1 - num2;
+            }
+            else if (type == "*")
+            {
+                sum = num1 * num2;
+            }
+            else if (type == "/")
+            {
+                sum = num1 / num2;
+            }
+            return sum;
+        }
+    }
+}
---
+++ homework1/calculator1/calculator1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入第一个数字：");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入第二个数字：");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入要计算的类型：");
+            string type = Console.ReadLine();
+
+            Calculate calculate = new Calculate();
+            int sum = calculate.GetResult(type, num1, num2);
+            Console.WriteLine("计算结果为：{0}",sum);
+            Console.ReadKey();
+        }
+    }
+}
