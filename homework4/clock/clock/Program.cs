75bd0ef175401d0cf9024c7d695e7fd588970581
zblzbl347 <61130402+zblzbl347@users.noreply.github.com>
2020-05-26 23:37:03 +0800

    Add files via upload

---
+++ homework4/clock/clock/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+namespace clock
+{
+    class Clock
+    {
+        public delegate void ClickHanlder();
+        public delegate void AlarmHanlder();
+        public event ClickHanlder ClockClick;
+        public event AlarmHanlder ClockAlarm;
+        public int hour, minute, second = 0;
+        public void SetAlarm(int h, int m, int s)
+        {
+            hour = h;
+            minute = m;
+            second = s;
+        }
+        public void Click()
+        {
+            ClockClick();
+        }
+        public void Alarm()
+        {
+            ClockAlarm();
+        }
+        public void Start()
+        {
+            while (true)
+            {
+                Click();
+                int nowhour = DateTime.Now.Hour;
+                int nowminute = DateTime.Now.Minute;
+                int nowsecond = DateTime.Now.Second;
+                if (hour == nowhour && minute == nowminute && second == nowsecond)
+                {
+                    Alarm();
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock myClock = new Clock();
+            myClock.ClockClick += Ticking;
+            myClock.ClockAlarm += Alarming;
+            myClock.SetAlarm(13, 02, 00);
+            myClock.Start();
+        }
+        public static void Alarming()
+        {
+            Console.WriteLine("叮铃铃铃！");
+        }
+        public static void Ticking()
+        {
+            Console.WriteLine("嘀嗒");
+        }
+    }
+}
---
+++ homework4/list/list/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace list
+{
+
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void Foreach(Action<T> action)
+        {
+            for (Node<T> p = head; p != null; p = p.Next)
+            {
+                action(p.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            GenericList<int> list = new GenericList<int>();
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(i + random.Next(0, 10));
+            }
+            list.Foreach(data => Console.Write(data + "  "));
+            Console.WriteLine();
+            list.Foreach(data => max = data > max ? data : max);
+            list.Foreach(data => min = data < min ? data : min);
+            list.Foreach(data => sum += data);
+            Console.WriteLine($"最大值为：{max}，最小值为：{min}，和为：{sum}");
+        }
+
+    }
+
+}
